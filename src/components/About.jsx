2e5ae9edba80e72f51c8f91d6aebfca4e6eaa611
Shriya Sudhakar <ss3576@cornell.edu>
2024-11-14 03:20:19 -0500

    add email address to about

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,7 +4,7 @@ const About = () => {
   return (
     <div id='about' className='flex min-h-screen w-full flex-col items-center justify-center gap-20 p-14 md:p-20 lg:p-36'>
       <h1 className='text-center text-5xl font-light'>About</h1>
-      <p className='text-left'>Hello! My name is Shriya Sudhakar, and I'm a current Sophomore at Cornell University studying Computer Science and Artificial Intelligence. This past summer, I interned at Google on Mendel Lifecycle as a Full-Stack Engineer. The previous summer, I was at Bloomberg as a SWE Intern. I'm super involved at Cornell through many organizations such as WICC, Cornell Mars Rover, and Cornell Orchestras.</p>
+      <p className='text-left'>Hello! My name is Shriya Sudhakar, and I'm a current Sophomore at Cornell University studying Computer Science and Artificial Intelligence. This past summer, I interned at Google on Mendel Lifecycle as a Full-Stack Engineer. The previous summer, I was at Bloomberg as a SWE Intern. I'm super involved at Cornell through many organizations such as WICC, Cornell Mars Rover, and Cornell Orchestras. Feel free to reach out at ss3576@cornell.edu!</p>
     </div>
   )
 }
