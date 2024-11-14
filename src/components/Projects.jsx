86d296ddb8d7909d5826e87edf6ca18a28385e20
Shriya Sudhakar <ss3576@cornell.edu>
2024-11-14 03:00:21 -0500

    changed formatting for projects

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -4,7 +4,7 @@ const Projects = () => {
   return (
     <div id='projects' className='min-h-screen w-full flex flex-col items-center justify-center gap-20 p-10 md:p-16 xl:px-32'>
       <h1 className='text-center text-5xl font-light'>Projects</h1>
-      <div className='grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-5'>
+      <div className='grid grid-cols-w lg:grid-cols-2 gap-5'>
         <div className="text-left space-y-2 border-2 hover:scale-105 transition-all duration-200 cursor-pointer px-8 py-10 border-indigo-600 rounded-lg hover:bg-indigo-60">
           <h1 className='text-3xl font-semibold'>Cornell Mars Rover BEV</h1>
           <h3 className='text-l text-gray-700'>Controls Software Member</h3>
